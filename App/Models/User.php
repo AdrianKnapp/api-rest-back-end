3ad85890101636ad8961509312059a57ef695598
Adrian <adrknapp@gmail.com>
2021-08-31 21:15:43 -0300

    First commit

---
+++ App/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+    namespace App\Models;
+
+    class User
+    {
+        private static $table = 'user';
+
+        public static function select(int $id) {
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'SELECT * FROM '.self::$table.' WHERE id = :id';
+            $stmt = $connPdo->prepare($sql);
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetch(\PDO::FETCH_ASSOC);
+            } else {
+                throw new \Exception("User not found.");
+            }
+        }
+
+        public static function selectAll() {
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'SELECT * FROM '.self::$table;
+            $stmt = $connPdo->prepare($sql);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            } else {
+                throw new \Exception("User not found");
+            }
+        }
+
+        public static function insert($data) {
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+
+            $sql = 'INSERT INTO '.self::$table.' (email, password, name) VALUES (:em, :pa, :na)';
+            $stmt = $connPdo->prepare($sql);
+            $stmt->bindValue(':em', $data['email']);
+            $stmt->bindValue(':pa', $data['password']);
+            $stmt->bindValue(':na', $data['name']);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return 'User inserted.';
+            } else {
+                throw new \Exception("Insert user error.");
+            }
+        }
+    }
